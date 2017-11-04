6bff1fcf8d88d3a1c27fbe1c893940e38778edde
Ben <ben@ben-rush.net>
2017-11-04 13:30:08 -0500

    Works

--- Explorations/SimpleMLP/MLP/NetworkTrainer.cs
+++ Explorations/SimpleMLP/MLP/NetworkTrainer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,33 +12,36 @@ namespace SimpleMLP.MLP
         public void Train(Network network, List<TrainingData> trainingData, double stepSize, int numberOfIterations)
         {
             int trainingDataLength = trainingData.Count;
-
-            for (int iterationCount = 0; iterationCount < numberOfIterations; iterationCount++)
+            //using (StreamWriter fout = File.CreateText("c:/users/ben/desktop/turd.csv"))
             {
-                double debugAverageOutputError = 0.0;
-                // iterate over each instance of the training data. 
-                for (int t = 0; t < trainingDataLength; t++)
+                for (int iterationCount = 0; iterationCount < numberOfIterations; iterationCount++)
                 {
-                    // get an instance of training data. 
-                    TrainingData data = trainingData[t];
+                    double debugAverageOutputError = 0.0;
+                    // iterate over each instance of the training data. 
+                    for (int t = 0; t < trainingDataLength; t++)
+                    {
+                        // get an instance of training data. 
+                        TrainingData data = trainingData[t];
 
-                    // set the input to the net. 
-                    network.SetInputLayer(data.X);
-                    // feed forward. 
-                    network.Feedforward();
-                    // back propagation
-                    debugAverageOutputError += network.Backpropagation(data.Y);
+                        // set the input to the net. 
+                        network.SetInputLayer(data.X);
+                        // feed forward. 
+                        network.Feedforward();
+                        // back propagation
+                        debugAverageOutputError += network.Backpropagation(data.Y);
+                    }
+                    if(iterationCount % 1000 == 0)
+                    {
+                        //fout.WriteLine(debugAverageOutputError / (trainingDataLength * 1.0)); 
+                        Console.WriteLine($"Iteration {iterationCount}, network error: {debugAverageOutputError / (trainingDataLength * 1.0)}");
+                    }
+
+                    // update the network. 
+                    network.UpdateNetwork(stepSize);
+
+                    //Console.WriteLine($"\t0: {network.Execute(new double[1] { 0 })[0]}");
+                    //Console.WriteLine($"\t1: {network.Execute(new double[1] { 1 })[0]}");
                 }
-                if(iterationCount % 100 == 0)
-                {
-                    Console.WriteLine($"Iteration {iterationCount}, network error: {debugAverageOutputError / (trainingDataLength * 1.0)}");
-                }
-
-                // update the network. 
-                network.UpdateNetwork(stepSize);
-
-                //Console.WriteLine($"\t0: {network.Execute(new double[1] { 0 })[0]}");
-                //Console.WriteLine($"\t1: {network.Execute(new double[1] { 1 })[0]}");
             }
         }
     }
--- Explorations/SimpleMLP/Program.cs
+++ Explorations/SimpleMLP/Program.cs
@@ -14,35 +14,35 @@ namespace SimpleMLP
         {
             List<TrainingData> trainingData = new List<TrainingData>();
 
+            //trainingData.Add(new TrainingData
+            //{
+            //    X = new double[2] { .05, .1 },
+            //    Y = new double[2] { .01, .99 }
+            //});
+
             trainingData.Add(new TrainingData
             {
-                X = new double[2] { .05, .1 },
-                Y = new double[2] { .01, .99 }
+                X = new double[2] { 0, 0 },
+                Y = new double[1] { 0 }
             });
 
-            //trainingData.Add(new TrainingData
-            //{
-            //    X = new double[2] { 0, 0 },
-            //    Y = new double[1] { 0 }
-            //});
+            trainingData.Add(new TrainingData
+            {
+                X = new double[2] { 1, 1 },
+                Y = new double[1] { 0 }
+            });
 
-            //trainingData.Add(new TrainingData
-            //{
-            //    X = new double[2] { 1, 1 },
-            //    Y = new double[1] { 0 }
-            //});
+            trainingData.Add(new TrainingData
+            {
+                X = new double[2] { 1, 0 },
+                Y = new double[1] { 1 }
+            });
 
-            //trainingData.Add(new TrainingData
-            //{
-            //    X = new double[2] { 1, 0 },
-            //    Y = new double[1] { 1 }
-            //});
-
-            //trainingData.Add(new TrainingData
-            //{
-            //    X = new double[2] { 0, 1 },
-            //    Y = new double[1] { 1 }
-            //});
+            trainingData.Add(new TrainingData
+            {
+                X = new double[2] { 0, 1 },
+                Y = new double[1] { 1 }
+            });
 
             return trainingData;
         }
@@ -52,24 +52,24 @@ namespace SimpleMLP
             // What I cannot create, I do not understand. 
             // ~Richard P. Feynman
 
-            Network network = Network.BuildNetwork(2, 2, 2);
+            Network network = Network.BuildNetwork(2, 1, 5, 10, 15);
 
             // hackery for testing. 
-            network.HiddenLayers[0].Neurons[0].Dendrites[0].Weight = .15;
-            network.HiddenLayers[0].Neurons[0].Dendrites[1].Weight = .2;
-            network.HiddenLayers[0].Neurons[1].Dendrites[0].Weight = .25;
-            network.HiddenLayers[0].Neurons[1].Dendrites[1].Weight = .30;
+            //network.HiddenLayers[0].Neurons[0].Dendrites[0].Weight = .15;
+            //network.HiddenLayers[0].Neurons[0].Dendrites[1].Weight = .2;
+            //network.HiddenLayers[0].Neurons[1].Dendrites[0].Weight = .25;
+            //network.HiddenLayers[0].Neurons[1].Dendrites[1].Weight = .30;
 
-            network.HiddenLayers[0].Neurons[0].Bias = .35;
-            network.HiddenLayers[0].Neurons[1].Bias = .35;
+            //network.HiddenLayers[0].Neurons[0].Bias = .35;
+            //network.HiddenLayers[0].Neurons[1].Bias = .35;
 
-            network.OutputLayer.Neurons[0].Dendrites[0].Weight = .4;
-            network.OutputLayer.Neurons[0].Dendrites[1].Weight = .45;
-            network.OutputLayer.Neurons[1].Dendrites[0].Weight = .50;
-            network.OutputLayer.Neurons[1].Dendrites[1].Weight = .55;
+            //network.OutputLayer.Neurons[0].Dendrites[0].Weight = .4;
+            //network.OutputLayer.Neurons[0].Dendrites[1].Weight = .45;
+            //network.OutputLayer.Neurons[1].Dendrites[0].Weight = .50;
+            //network.OutputLayer.Neurons[1].Dendrites[1].Weight = .55;
 
-            network.OutputLayer.Neurons[0].Bias = .6;
-            network.OutputLayer.Neurons[1].Bias = .6;
+            //network.OutputLayer.Neurons[0].Bias = .6;
+            //network.OutputLayer.Neurons[1].Bias = .6;
 
             NetworkInDGML dgmlRepresentation = NetworkInDGML.Create(network);
             dgmlRepresentation.Serialize("networkTopology.dgml");
@@ -77,7 +77,7 @@ namespace SimpleMLP
             List<TrainingData> trainingData = BuildTrainingData();
 
             NetworkTrainer networkTrainer = new NetworkTrainer();
-            networkTrainer.Train(network, trainingData, .5, 50000);
+            networkTrainer.Train(network, trainingData, .5, 1000000);
 
             foreach (TrainingData data in trainingData)
             {
